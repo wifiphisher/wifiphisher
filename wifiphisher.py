85ee9ab77d1000da005e6d14abee0fc46555c802
George Chatzisofroniou <sophron@latthi.com>
2015-01-04 15:26:45 +0200

    Fixed doc.

--- wifiphisher.py
+++ wifiphisher.py
@@ -26,7 +26,7 @@ conf.verb = 0
 PORT = 8081
 SSL_PORT = 444
 PEM = 'cert/server.pem'
-PHISING_PAGE = "access-point-pages/dlink"
+PHISING_PAGE = "access-point-pages/minimal"
 DN = open(os.devnull, 'w')
 
 # Console colors
@@ -49,8 +49,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--channel", help="Choose the channel for monitoring. Default is channel 1", default="1")
     parser.add_argument("-s", "--skip", help="Skip deauthing this MAC address. Example: -s 00:11:BB:33:44:AA")
-    parser.add_argument("-jI", "--jamminginterface", help="Choose monitor mode interface. By default script will find the most powerful interface and starts monitor mode on it. Example: -i mon5")
-    parser.add_argument("-aI", "--apinterface", help="Choose monitor mode interface. By default script will find the most powerful interface and starts monitor mode on it. Example: -i mon5")
+    parser.add_argument("-jI", "--jamminginterface", help="Choose monitor mode interface. By default script will find the most powerful interface and starts monitor mode on it. Example: -jI mon5")
+    parser.add_argument("-aI", "--apinterface", help="Choose monitor mode interface. By default script will find the second most powerful interface and starts monitor mode on it. Example: -aI mon5")
     parser.add_argument("-m", "--maximum", help="Choose the maximum number of clients to deauth. List of clients will be emptied and repopulated after hitting the limit. Example: -m 5")
     parser.add_argument("-n", "--noupdate", help="Do not clear the deauth list when the maximum (-m) number of client/AP combos is reached. Must be used in conjunction with -m. Example: -m 10 -n", action='store_true')
     parser.add_argument("-t", "--timeinterval", help="Choose the time interval between packets being sent. Default is as fast as possible. If you see scapy errors like 'no buffer space' try: -t .00001")
