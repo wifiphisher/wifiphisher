33fc2789abdb9e697538ae01ca73cb85644f153c
Hassen ben tanfous <hbt.youtube@Yahoo.com>
2015-03-06 20:57:55 +0100

    Typo

--- wifiphisher.py
+++ wifiphisher.py
@@ -872,11 +872,11 @@ def get_hostapd():
     if not os.path.isfile('/usr/sbin/hostapd'):
         sys.exit((
             '\n[' + R + '-' + W + '] Unable to install the \'hostapd\' package!\n' +
-            '[' + T + '*' + W + '] This process requires a persistent internet connexion!\n' +
+            '[' + T + '*' + W + '] This process requires a persistent internet connection!\n' +
             'Please follow the link below to configure your sources.list\n' +
             B + 'http://docs.kali.org/general-use/kali-linux-sources-list-repositories\n' + W +
-            '[' + G + ' + ' + W + '] Run apt-get update for changes to take effect.\n' +
-            '[' + G + ' + ' + W + '] Rerun the script again to install hostapd.\n' +
+            '[' + G + '+' + W + '] Run apt-get update for changes to take effect.\n' +
+            '[' + G + '+' + W + '] Rerun the script again to install hostapd.\n' +
             '[' + R + '!' + W + '] Closing'
          ))
 
