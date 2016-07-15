6d6894d1f9b11cf79930814a42d787af849bb9db
George Chatzisofroniou <sophron@latthi.com>
2016-07-15 15:25:18 -0400

    Setup is responsible for missing packages.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import sys
+import os
 from setuptools import setup, find_packages
+from distutils.spawn import find_executable
+from wifiphisher.constants import *
 setup(
 name = "wifiphisher",
 author = "sophron",
@@ -20,6 +23,93 @@ install_requires = [
 'PyRIC',
 'jinja2']
 )
+
+def get_dnsmasq():
+    if not os.path.isfile('/usr/sbin/dnsmasq'):
+        install = raw_input(
+            ('[' + T + '*' + W + '] dnsmasq not found ' +
+             'in /usr/bin/dnsmasq, install now? [y/n] ')
+        )
+        if install == 'y':
+            if os.path.isfile('/usr/bin/pacman'):
+                os.system('pacman -S dnsmasq')
+            elif os.path.isfile('/usr/bin/yum'):
+                os.system('yum install dnsmasq')
+            else:
+                os.system('apt-get -y install dnsmasq')
+        else:
+            sys.exit(('[' + R + '-' + W + '] dnsmasq' +
+                     ' not found in /usr/sbin/dnsmasq'))
+    if not os.path.isfile('/usr/sbin/dnsmasq'):
+        sys.exit((
+            '\n[' + R + '-' + W + '] Unable to install the \'dnsmasq\' package!\n' +
+            '[' + T + '*' + W + '] This process requires a persistent internet connection!\n' +
+            'Please follow the link below to configure your sources.list\n' +
+            B + 'http://docs.kali.org/general-use/kali-linux-sources-list-repositories\n' + W +
+            '[' + G + '+' + W + '] Run apt-get update for changes to take effect.\n' +
+            '[' + G + '+' + W + '] Rerun the script to install dnsmasq.\n' +
+            '[' + R + '!' + W + '] Closing'
+         ))
+
+def get_hostapd():
+    if not os.path.isfile('/usr/sbin/hostapd'):
+        install = raw_input(
+            ('[' + T + '*' + W + '] hostapd not found ' +
+             'in /usr/sbin/hostapd, install now? [y/n] ')
+        )
+        if install == 'y':
+            if os.path.isfile('/usr/bin/pacman'):
+                os.system('pacman -S hostapd')
+            elif os.path.isfile('/usr/bin/yum'):
+                os.system('yum install hostapd')
+            else:
+                os.system('apt-get -y install hostapd')
+        else:
+            sys.exit(('[' + R + '-' + W + '] hostapd' +
+                     ' not found in /usr/sbin/hostapd'))
+    if not os.path.isfile('/usr/sbin/hostapd'):
+        sys.exit((
+            '\n[' + R + '-' + W + '] Unable to install the \'hostapd\' package!\n' +
+            '[' + T + '*' + W + '] This process requires a persistent internet connection!\n' +
+            'Please follow the link below to configure your sources.list\n' +
+            B + 'http://docs.kali.org/general-use/kali-linux-sources-list-repositories\n' + W +
+            '[' + G + '+' + W + '] Run apt-get update for changes to take effect.\n' +
+            '[' + G + '+' + W + '] Rerun the script to install hostapd.\n' +
+            '[' + R + '!' + W + '] Closing'
+         ))
+
+def get_ifconfig():
+    # This is only useful for Arch Linux which does not contain ifconfig by default
+    if not find_executable('ifconfig'):
+        install = raw_input(
+            ('[' + T + '*' + W + '] ifconfig not found. ' +
+             'install now? [y/n] ')
+        )
+        if install == 'y':
+            if os.path.isfile('/usr/bin/pacman'):
+                os.system('pacman -S net-tools')
+            else:
+                sys.exit((
+                    '\n[' + R + '-' + W + '] Don\'t know how to install ifconfig for your distribution.\n' +
+                    '[' + G + '+' + W + '] Rerun the script after installing it manually.\n' +
+                    '[' + R + '!' + W + '] Closing'
+                ))
+        else:
+            sys.exit(('[' + R + '-' + W + '] ifconfig' +
+                     ' not found'))
+    if not find_executable('ifconfig'):
+        sys.exit((
+            '\n[' + R + '-' + W + '] Unable to install the \'net-tools\' package!\n' +
+            '[' + T + '*' + W + '] This process requires a persistent internet connection!\n' +
+            '[' + G + '+' + W + '] Run pacman -Syu to make sure you are up to date first.\n' +
+            '[' + G + '+' + W + '] Rerun the script to install net-tools.\n' +
+            '[' + R + '!' + W + '] Closing'
+         ))
+
+# Get hostapd, dnsmasq or ifconfig if needed
+get_hostapd()
+get_dnsmasq()
+get_ifconfig()
 
 print
 print "                     _  __ _       _     _     _               "
