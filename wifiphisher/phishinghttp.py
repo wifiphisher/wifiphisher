2687dc91e1e9f2cd4035dc2ba5e10b9ccecbec37
George Chatzisofroniou <sophron@latthi.com>
2016-11-26 15:58:07 -0500

    One slash after a dot, messes things.

--- wifiphisher/phishinghttp.py
+++ wifiphisher/phishinghttp.py
@@ -104,7 +104,7 @@ class HTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         global template
 
         # Two slashes in the URL will confuse our server
-        if self.path.split('.')[-1].count("/") > 1:
+        if self.path.split('.')[-1].count("/") > 0:
             self.redirect("/index.html")
 
         template_path = template.get_path()
