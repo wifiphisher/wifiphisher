578b11ae2b27c9e8789c360cd8f74dfd2a558a2d
George Chatzisofroniou <sophron@latthi.com>
2017-05-12 06:12:50 -0400

    Updated tests, pep8 fixes.

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -1,3 +1,7 @@
+"""
+Unit tests for Extension Manager
+"""
+
 import sys
 import os
 import unittest
@@ -28,9 +32,6 @@ class Extension1(object):
 
     def send_output(self):
         return ["one", "two"]
-
-    def on_exit(self):
-        pass
 """
 
 CONTENTS_EXTENSION_2 = """
@@ -48,10 +49,8 @@ class Extension2(object):
 
     def send_output(self):
         return ["three", "four", "five"]
-
-    def on_exit(self):
-        pass
 """
+
 
 class TestExtensionManager(unittest.TestCase):
 
@@ -67,53 +66,64 @@ class TestExtensionManager(unittest.TestCase):
             f.write(CONTENTS_EXTENSION_2)
             f.close()
 
-    @mock.patch("wifiphisher.common.constants.EXTENSIONS", ["extension1"])    
-    @mock.patch("wifiphisher.common.constants.EXTENSIONS_LOADPATH", "tests.extensions.")
-    @mock.patch("wifiphisher.common.extensions.ExtensionManager._send", return_value=0)
-    def test_single_extension(self, send_func):
+    @mock.patch("wifiphisher.common.constants.EXTENSIONS", ["extension1"])
+    @mock.patch(
+        "wifiphisher.common.constants.EXTENSIONS_LOADPATH",
+        "tests.extensions.")
+    def test_single_extension(self):
         # Init an EM and pass some shared data
-        self.em = extensions.ExtensionManager()
+        em = extensions.ExtensionManager()
+        em.set_interface("wlan0")
         shared_data = {"one": 1, "two": 2}
-        self.em.init_extensions(shared_data)
-        assert not send_func.called
-        self.em.start_extensions()
+        em.init_extensions(shared_data)
         # A deauth packet appears in the air
-        packet = (dot11.RadioTap() / dot11.Dot11(type=0, subtype=12, \
-                addr1="00:00:00:00:00:00", addr2="00:00:00:00:00:00", addr3="00:00:00:00:00:00") \
-              / dot11.Dot11Deauth())
-        self.em._process_packet(packet)
-        # After EM starts, the send daemon is triggered and
-        # sends any packets from extensions
-        assert send_func.called == 1
+        packet = (
+            dot11.RadioTap() /
+            dot11.Dot11(
+                type=0,
+                subtype=12,
+                addr1="00:00:00:00:00:00",
+                addr2="00:00:00:00:00:00",
+                addr3="00:00:00:00:00:00") /
+            dot11.Dot11Deauth())
+        em._process_packet(packet)
         # The extension1.py sent packet "1" and returned output
         # "one", "two". Validate with get_packet(), send_output()
-        assert self.em._packets_to_send == [1] 
-        assert self.em.get_output() == ["one", "two"]
+        assert em._packets_to_send == [1]
+        assert em.get_output() == ["one", "two"]
 
-    @mock.patch("wifiphisher.common.constants.EXTENSIONS", ["extension1", "extension2"])    
-    @mock.patch("wifiphisher.common.constants.EXTENSIONS_LOADPATH", "tests.extensions.")
-    @mock.patch("wifiphisher.common.extensions.ExtensionManager._send", return_value=0)
-    def test_multiple_extensions(self, send_func):
+    @mock.patch(
+        "wifiphisher.common.constants.EXTENSIONS", [
+            "extension1", "extension2"])
+    @mock.patch(
+        "wifiphisher.common.constants.EXTENSIONS_LOADPATH",
+        "tests.extensions.")
+    def test_multiple_extensions(self):
         # Init an EM and pass some shared data
-        self.em = extensions.ExtensionManager()
+        em = extensions.ExtensionManager()
         shared_data = {"one": 1, "two": 2}
-        self.em.init_extensions(shared_data)
-        self.em.start_extensions()
+        em.init_extensions(shared_data)
         # A deauth packet appears in the air
-        packet = (dot11.RadioTap() / dot11.Dot11(type=0, subtype=12, \
-                addr1="00:00:00:00:00:00", addr2="00:00:00:00:00:00", addr3="00:00:00:00:00:00") \
-              / dot11.Dot11Deauth())
-        self.em._process_packet(packet)
+        packet = (
+            dot11.RadioTap() /
+            dot11.Dot11(
+                type=0,
+                subtype=12,
+                addr1="00:00:00:00:00:00",
+                addr2="00:00:00:00:00:00",
+                addr3="00:00:00:00:00:00") /
+            dot11.Dot11Deauth())
+        em._process_packet(packet)
         # Packets to send have been merged from the two extensions
         # Validate with get_packet()
-        assert self.em._packets_to_send == [1, 2, 3, 4]
+        assert em._packets_to_send == [1, 2, 3, 4]
         # Output has also been merged in one list.
         # Validate with send_output()
-        assert self.em.get_output() == ["one", "two", "three", "four", "five"] 
- 
+        assert em.get_output() == ["one", "two", "three", "four", "five"]
+
     def tearDown(self):
-        self.em.on_exit()
         shutil.rmtree("tests/extensions")
+
 
 if __name__ == "__main__":
     unittest.main()
