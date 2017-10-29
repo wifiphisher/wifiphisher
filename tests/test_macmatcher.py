a5879a74943b520687629c30b95789db15cd158e
Brian Smith <brian.smith@riseup.net>
2017-10-29 19:43:56 -0400

    Add function to get vendor's information
    Add a function to get the vendor's name based on the MAC address of the
    access point. Test cases for the function were also added.

--- tests/test_macmatcher.py
+++ tests/test_macmatcher.py
@@ -18,3 +18,31 @@ def test_parse_oui_file(mock_open):
     message = "Failed to parse the oui file"
 
     assert actual == expected, message
+
+
+def test_get_vendor_1():
+    """
+    Test get_vendor function when MAC address is known
+    """
+    vendor_name = "Fake Inc."
+    vendors = {"112233": vendor_name}
+    mac_address = "11:22:33:44:55:66"
+
+    result = macmatcher.get_vendor(vendors, mac_address)
+    message = "Failed to get correct vendor"
+
+    assert result == vendor_name, message
+
+
+def test_get_vendor_2():
+    """
+    Test get_vendor function when MAC address is unknown
+    """
+    vendor_name = "Fake Inc."
+    vendors = {"112233": vendor_name}
+    mac_address = "00:11:22:33:44:55"
+
+    result = macmatcher.get_vendor(vendors, mac_address)
+    message = "Failed to get correct vendor"
+
+    assert result == "Unknown", message
