4b88d89a8633a86c53a9e5ddbaf14e812cfde23c
Brian Smith <brian.smith@riseup.net>
2017-11-11 18:36:32 -0500

    Add the namedtuple result in the constants
    Add the namedtupled result to the constants so all function can access
    it.

--- wifiphisher/common/constants.py
+++ wifiphisher/common/constants.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #pylint: skip-file
 import os
+import collections
 
 dir_of_executable = os.path.dirname(__file__)
 path_to_project_root = os.path.abspath(
@@ -132,3 +133,5 @@ AP_SEL_ATTRS = 'interface mac_matcher network_manager args'
 
 # Fourway handshake extension
 CONST_A = "Pairwise key expansion"
+
+RESULT = collections.namedtuple("Result", "status, error_message")
