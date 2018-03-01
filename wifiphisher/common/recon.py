wifiphisher/wifiphisher

commit 24d8d4c3fceae4e9b9d0ec7c35a414826b5d014e

refactor: recon module
Refactor recon module to remove duplicate and unnecessary code.

wifiphisher/common/recon.py
@@ -10,7 +10,7 @@ import logging
 import scapy.layers.dot11 as dot11
 import wifiphisher.common.constants as constants
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 class AccessPoint(object):
@@ -32,90 +32,16 @@ class AccessPoint(object):
         :type encryption: string
         """
 
-        self._name = ssid
-        self._mac_address = bssid
-        self._channel = channel
-        self._encryption = encryption
-        self._signal_strength = None
+        self.name = ssid
+        self.mac_address = bssid
+        self.channel = channel
+        self.encryption = encryption
+        self.signal_strength = None
         self._clients = set()
 
         if capture_file:
             with open(capture_file, "a") as f:
                 f.write(bssid + " " + ssid + "\n")
-
-    def get_name(self):
-        """
-        Return the name(ESSID) of the access point
-
-        :param self: An AccessPoint object
-        :type self: AccessPoint
-        :return: Name of the access point
-        :rtype: string
-        """
-
-        return self._name
-
-    def get_mac_address(self):
-        """
-        Return the MAC address(BSSID) of the access point
-
-        :param self: An AccessPoint object
-        :type self: AccessPoint
-        :return: MAC address of the access point
-        :rtype: string
-        """
-
-        return self._mac_address
-
-    def get_channel(self):
-        """
-        Return the channel of the access point
-
-        :param self: An AccessPoint object
-        :type self: AccessPoint
-        :return: Channel of the access point
-        :rtype: string
-        """
-
-        return self._channel
-
-    def get_encryption(self):
-        """
-        Return the encryption type of the access point
-
-        :param self: An AccessPoint object
-        :type self: AccessPoint
-        :return: Encryption type of the access point
-        :rtype: string
-        """
-
-        return self._encryption
-
-    def get_signal_strength(self):
-        """
-        Return the access point's signal strength
-
-        :param self: An AccessPoint object
-        :type self: AccessPoint
-        :return: Access point's singnal strength
-        :rtype: string
-        """
-
-        return self._signal_strength
-
-    def set_signal_strength(self, power):
-        """
-        Set the access point's sinnal strength
-
-        :param self: An AccessPoint object
-        :param power: The signal strength of access point
-        :type self: AccessPoint
-        :type power: string
-        :return: None
-        :rtype: None
-        """
-
-        self._signal_strength = power
 
     def add_client(self, client):
         """
@@ -162,7 +88,7 @@ class AccessPointFinder(object):
         """
 
         self._interface = ap_interface
-        self._observed_access_points = list()
+        self.observed_access_points = list()
         self._capture_file = False
         self._should_continue = True
         self._hidden_networks = list()
@@ -211,22 +137,6 @@ class AccessPointFinder(object):
         elif packet.haslayer(dot11.Dot11):
             self._find_clients(packet)
 
-    def _parse_rssi(self, packet):
-        """
-        Parse the rssi info from the packet
-
-        :param self: An AccessPointFinder object
-        :param packet: A scapy.layers.RadioTap object
-        :type self: AccessPointFinder
-        :type packet: scapy.layers.RadioTap
-        :return: rssi
-        :rtype: int
-        """
-        tmp = ord(packet.notdecoded[-4:-3])
-        tmp1 = ord(packet.notdecoded[-2:-1])
-        rssi = -(256 - max(tmp, tmp1))
-        return rssi
-
     def _create_ap_with_info(self, packet):
         """
         Create and add an access point using the extracted information
@@ -255,8 +165,8 @@ class AccessPointFinder(object):
         non_decodable_name = "<contains non-printable chars>"
 
         # find the signal strength
-        rssi = self._parse_rssi(packet)
-        new_signal_strength = self._calculate_signal_strength(rssi)
+        rssi = get_rssi(packet.notdecoded)
+        new_signal_strength = calculate_signal_strength(rssi)
 
         # get the name of the access point
         # if the name is no utf8 compatible use pre set name
@@ -267,21 +177,20 @@ class AccessPointFinder(object):
 
         # just update signal strength in case of discovered
         # access point
-        for access_point in self._observed_access_points:
-            if mac_address == access_point.get_mac_address():
+        for access_point in self.observed_access_points:
+            if mac_address == access_point.mac_address:
                 # find the current and calculate the difference
-                current_signal_strength = access_point.get_signal_strength()
-                signal_strength_difference = new_signal_strength -\
-                    current_signal_strength
+                current_signal_strength = access_point.signal_strength
+                signal_difference = new_signal_strength - current_signal_strength
 
-                # update signal strength if more than 5% difference
-                if signal_strength_difference > 5:
-                    access_point.set_signal_strength(new_signal_strength)
+                # update signal strength if difference is greater than 5
+                if signal_difference > 5:
+                    access_point.signal_strength = new_signal_strength
 
                 return None
 
         # get encryption type
-        encryption_type = self._find_encryption_type(packet)
+        encryption_type = find_encryption_type(packet)
 
         # with all the information gathered create and add the
         # access point
@@ -291,59 +200,8 @@ class AccessPointFinder(object):
             channel,
             encryption_type,
             capture_file=self._capture_file)
-        access_point.set_signal_strength(new_signal_strength)
-        self._observed_access_points.append(access_point)
-
-    def _find_encryption_type(self, packet):
-        """
-        Return the encryption type of the access point
-
-        :param self: An AccessPointFinder object
-        :param packet: A scapy.layers.RadioTap object
-        :type self: AccessPointFinder
-        :type packet: scapy.layers.RadioTap
-        :return: encryption type of the access point
-        :rtype: string
-        .. note: Possible return values are WPA2, WPA, WEP and OPEN
-        """
-
-        encryption_info = packet.sprintf("%Dot11Beacon.cap%")
-        elt_section = packet[dot11.Dot11Elt]
-        encryption_type = None
-        is_wps = False
-
-        # extract information from packet
-        while isinstance(elt_section, dot11.Dot11Elt):
-            # check if encryption type is WPA2
-            if elt_section.ID == 48:
-                encryption_type = "WPA2"
-
-            # check if encryption type is WPA
-            elif elt_section.ID == 221 and\
-                    elt_section.info.startswith("\x00P\xf2\x01\x01\x00"):
-                encryption_type = "WPA"
-            # check if WPS IE exists
-            if elt_section.ID == 221 and\
-                    elt_section.info.startswith("\x00P\xf2\x04"):
-                is_wps = True
-            # break if wps and security is found
-            if encryption_type and is_wps:
-                break
-
-            # break down the packet
-            elt_section = elt_section.payload
-
-        # check to see if encryption type is either WEP or OPEN
-        if not encryption_type:
-            if "privacy" in encryption_info:
-                encryption_type = "WEP"
-            else:
-                encryption_type = "OPEN"
-
-        if encryption_type != "WEP" and is_wps:
-            encryption_type += "/WPS"
-
-        return encryption_type
+        access_point.signal_strength = new_signal_strength
+        self.observed_access_points.append(access_point)
 
     def _sniff_packets(self):
         """
@@ -366,7 +224,7 @@ class AccessPointFinder(object):
     def capture_aps(self):
         self._capture_file = constants.LOCS_DIR + "area_" +\
             time.strftime("%Y%m%d_%H%M%S")
-        logger.info("Create lure10-capture file %s", self._capture_file)
+        LOGGER.info("Create lure10-capture file %s", self._capture_file)
 
     def find_all_access_points(self):
         """
@@ -402,19 +260,6 @@ class AccessPointFinder(object):
         self._channel_hop_thread.join(10)
         self._sniff_packets_thread.join(10)
 
-    def get_all_access_points(self):
-        """
-        Return a list of all access points
-
-        :param self: An AccessPointFinder object
-        :type self: AccessPointFinder
-        :return: list of access points
-        :rtype: list
-        .. note: A list of AccessPoint objects will be returned
-        """
-
-        return self._observed_access_points
-
     def _channel_hop(self):
         """
         Change the interface's channel every three seconds
@@ -436,26 +281,6 @@ class AccessPointFinder(object):
                     time.sleep(3)
                 else:
                     break
-
-    def _calculate_signal_strength(self, rssi):
-        """
-        calculate the signal strength of access point
-
-        :param self: An AccessPointFinder object
-        :type self: AccessPointFinder
-        :return: Signal strength of access point
-        :rtype: int
-        """
-
-        # calculate signal strength based on rssi value
-        if rssi <= -100:
-            signal_strength = 0
-        elif rssi >= -50:
-            signal_strength = 100
-        else:
-            signal_strength = 2 * (rssi + 100)
-
-        return signal_strength
 
     def _find_clients(self, packet):
         """
@@ -488,9 +313,9 @@ class AccessPointFinder(object):
 
             # if discovered access point is either sending or receving
             # add client if it's mac address is not in the MAC filter
-            for access_point in self._observed_access_points:
+            for access_point in self.observed_access_points:
                 # get the access point MAC address
-                access_point_mac = access_point.get_mac_address()
+                access_point_mac = access_point.mac_address
 
                 # in case access point is the reciever
                 # add sender as client
@@ -512,11 +337,89 @@ class AccessPointFinder(object):
         :rtype: None
         """
 
-        # sort access points in descending order based on
-        # signal strength
-        sorted_access_points = sorted(
-            self._observed_access_points,
-            key=lambda ap: ap.get_signal_strength(),
+        return sorted(
+            self.observed_access_points,
+            key=lambda ap: ap.signal_strength,
             reverse=True)
 
-        return sorted_access_points
+
+def get_rssi(non_decoded_packet):
+    """
+    Return the rssi value of the packet
+
+    :param packet: A scapy.layers.RadioTap object
+    :type packet: scapy.layers.RadioTap
+    :return: rssi value of packet
+    :rtype: int
+    """
+    return -(256 - max(
+        ord(non_decoded_packet[-4:-3]), ord(non_decoded_packet[-2:-1])))
+
+
+def calculate_signal_strength(rssi):
+    """
+    calculate the signal strength of access point
+
+    :param rssi: A rssi value
+    :type rssi: int
+    :return: Signal strength of access point
+    :rtype: int
+    """
+
+    if rssi <= -100:
+        signal_strength = 0
+    elif rssi >= -50:
+        signal_strength = 100
+    else:
+        signal_strength = 2 * (rssi + 100)
+
+    return signal_strength
+
+
+def find_encryption_type(packet):
+    """
+    Return the encryption type of the access point
+
+    :param packet: A scapy.layers.RadioTap object
+    :type packet: scapy.layers.RadioTap
+    :return: encryption type of the access point
+    :rtype: string
+    .. note: Possible return values are WPA2, WPA, WEP, OPEN,
+        WPA2/WPS and WPA/WPS
+    """
+
+    encryption_info = packet.sprintf("%Dot11Beacon.cap%")
+    elt_section = packet[dot11.Dot11Elt]
+    encryption_type = None
+    found_wps = False
+
+    # extract information from packet
+    while (isinstance(elt_section, dot11.Dot11Elt)
+           or (not encryption_type and not found_wps)):
+        # check if encryption type is WPA2
+        if elt_section.ID == 48:
+            encryption_type = "WPA2"
+
+        # check if encryption type is WPA
+        elif (elt_section.ID == 221
+              and elt_section.info.startswith("\x00P\xf2\x01\x01\x00")):
+            encryption_type = "WPA"
+        # check if WPS IE exists
+        if (elt_section.ID == 221
+                and elt_section.info.startswith("\x00P\xf2\x04")):
+            found_wps = True
+
+        # break down the packet
+        elt_section = elt_section.payload
+
+        # check to see if encryption type is either WEP or OPEN
+        if not encryption_type:
+            if "privacy" in encryption_info:
+                encryption_type = "WEP"
+            else:
+                encryption_type = "OPEN"
+
+        if encryption_type != "WEP" and found_wps:
+            encryption_type += "/WPS"
+
+        return encryption_type

wifiphisher/common/tui.py
@@ -185,8 +185,7 @@ class TuiTemplateSelection(object):
             display_str = "Options: [Up Arrow] Move Up  [Down Arrow] Move Down"
             screen.addstr(0, 0, display_string(max_window_len, display_str))
             display_str = "Available Phishing Scenarios:"
-            screen.addstr(3, 0,
-                          display_string(max_window_len, display_str),
+            screen.addstr(3, 0, display_string(max_window_len, display_str),
                           curses.A_BOLD)
         except curses.error:
             return 0
@@ -641,7 +640,7 @@ class TuiApSel(object):
 
         # check if any new access points have been discovered
         new_total_ap_number = len(
-            self.access_point_finder.get_all_access_points())
+            self.access_point_finder.observed_access_points)
 
         if new_total_ap_number != self.total_ap_number:
             self.access_points = self.access_point_finder.\
@@ -658,7 +657,7 @@ class TuiApSel(object):
         if ap_info.key == ord("\n") and self.total_ap_number != 0:
             # show message and exit
             screen.addstr(ap_info.max_h - 2, 3, "YOU HAVE SELECTED " +
-                          self.access_points[ap_info.pos - 1].get_name())
+                          self.access_points[ap_info.pos - 1].name)
             screen.refresh()
             time.sleep(1)
             is_apsel_end = True
@@ -743,17 +742,15 @@ class TuiApSel(object):
                 # get the access point and it's vendor
                 access_point = self.access_points[item_position - 1]
                 vendor = self.mac_matcher.get_vendor_name(
-                    access_point.get_mac_address())
+                    access_point.mac_address)
 
                 # the display format for showing access points
                 display_text = ((
                     "{0:30} {1:17} {2:2} {3:3}% {4:^7} {5:^5}"
                     " {6:20}").format(
-                        access_point.get_name(),
-                        access_point.get_mac_address(),
-                        access_point.get_channel(),
-                        access_point.get_signal_strength(),
-                        access_point.get_encryption(),
+                        access_point.name, access_point.mac_address,
+                        access_point.channel, access_point.signal_strength,
+                        access_point.encryption,
                         access_point.get_number_connected_clients(), vendor))
                 # shows whether the access point should be highlighted or not
                 # based on our current position

wifiphisher/pywifiphisher.py
@@ -444,10 +444,10 @@ class WifiphisherEngine:
             # otherwise shutdown
             if access_point:
                 # store choosen access point's information
-                essid = access_point.get_name()
-                channel = access_point.get_channel()
-                target_ap_mac = access_point.get_mac_address()
-                enctype = access_point.get_encryption()
+                essid = access_point.name
+                channel = access_point.channel
+                target_ap_mac = access_point.mac_address
+                enctype = access_point.encryption
             else:
                 self.stop()
         # create a template manager object

commit 7373bd24ab8188e5d03e216f247208d53a151326

Fix: indentation error
Fix an indentation error caused by previous commit 24d8d4c.

wifiphisher/common/recon.py
@@ -419,7 +419,7 @@ def find_encryption_type(packet):
             else:
                 encryption_type = "OPEN"
 
-        if encryption_type != "WEP" and found_wps:
-            encryption_type += "/WPS"
+    if encryption_type != "WEP" and found_wps:
+        encryption_type += "/WPS"
 
-        return encryption_type
+    return encryption_type