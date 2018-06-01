310fed1acc2bf74277fa48b2957f564cfa6c9434
Brian Smith <brian.smith@riseup.net>
2018-06-01 13:54:15 -0400

    Add support for including the readme file
    Add support to add the readme file to the source distribution. Also some extra tags are added.

--- setup.py
+++ setup.py
@@ -13,12 +13,16 @@ import wifiphisher.common.constants as constants
 class CleanCommand(Command):
     """Custom clean command to tidy up the project root."""
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
         os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
 
 def get_dnsmasq():
     """
@@ -29,8 +33,9 @@ def get_dnsmasq():
     """
 
     if not os.path.isfile("/usr/sbin/dnsmasq"):
-        install = raw_input(("[" + constants.T + "*" + constants.W + "] dnsmasq not found " +
-                             "in /usr/sbin/dnsmasq, " + "install now? [y/n] "))
+        install = raw_input(
+            ("[" + constants.T + "*" + constants.W + "] dnsmasq not found " +
+             "in /usr/sbin/dnsmasq, " + "install now? [y/n] "))
 
         if install == "y":
             if os.path.isfile("/usr/bin/pacman"):
@@ -44,49 +49,68 @@ def get_dnsmasq():
                       "not found in /usr/sbin/dnsmasq"))
 
     if not os.path.isfile("/usr/sbin/dnsmasq"):
-        dnsmasq_message = ("\n[" + constants.R + "-" + constants.W +
-                           "] Unable to install the \'dnsmasq\' package!\n" + "[" + constants.T +
-                           "*" + constants.W + "] This process requires a persistent internet " +
-                           "connection!\nPlease follow the link below to configure your " +
-                           "sources.list\n" + constants.B + "http://docs.kali.org/general-use/" +
-                           "kali-linux-sources-list-repositories\n" + constants.W + "[" +
-                           constants.G + "+" + constants.W + "] Run apt-get update for changes " +
-                           "to take effect.\n" + "[" + constants.G + "+" + constants.W + "] " +
-                           "Rerun the script to install dnsmasq.\n[" + constants.R + "!" +
-                           constants.W + "] Closing")
+        dnsmasq_message = (
+            "\n[" + constants.R + "-" + constants.W +
+            "] Unable to install the \'dnsmasq\' package!\n" + "[" +
+            constants.T + "*" + constants.W +
+            "] This process requires a persistent internet " +
+            "connection!\nPlease follow the link below to configure your " +
+            "sources.list\n" + constants.B +
+            "http://docs.kali.org/general-use/" +
+            "kali-linux-sources-list-repositories\n" + constants.W + "[" +
+            constants.G + "+" + constants.W +
+            "] Run apt-get update for changes " + "to take effect.\n" + "[" +
+            constants.G + "+" + constants.W + "] " +
+            "Rerun the script to install dnsmasq.\n[" + constants.R + "!" +
+            constants.W + "] Closing")
 
         sys.exit(dnsmasq_message)
 
-# setup settings
-NAME = "wifiphisher"
-AUTHOR = "sophron"
-AUTHOR_EMAIL = "sophron@latthi.com"
-URL = "https://github.com/wifiphisher/wifiphisher"
-DESCRIPTION = "Automated phishing attacks against Wi-Fi networks"
-LICENSE = "GPL"
-KEYWORDS = ["wifiphisher", "evil", "twin", "phishing"]
-PACKAGES = find_packages(exclude=["docs", "tests"])
-INCLUDE_PACKAGE_DATA = True
-VERSION = "1.4"
-CLASSIFIERS = ["Development Status :: 5 - Production/Stable",
-               "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-               "Natural Language :: English", "Operating System :: Unix",
-               "Programming Language :: Python :: 2", "Programming Language :: Python :: 2.7",
-               "Programming Language :: Python :: 2 :: Only", "Topic :: Security",
-               "Topic :: System :: Networking", "Intended Audience :: End Users/Desktop",
-               "Intended Audience :: System Administrators",
-               "Intended Audience :: Information Technology"]
-ENTRY_POINTS = {"console_scripts": ["wifiphisher = wifiphisher.pywifiphisher:run"]}
-# WORKAROUND: Download tornado 4.5.3 instead of latest so travis won't complain
-INSTALL_REQUIRES = ["PyRIC", "tornado==4.5.3",
-                    "pbkdf2", "roguehostapd", "scapy"]
-CMDCLASS = {"clean": CleanCommand,}
 
-# run setup
-setup(name=NAME, author=AUTHOR, author_email=AUTHOR_EMAIL, description=DESCRIPTION,
-      license=LICENSE, keywords=KEYWORDS, packages=PACKAGES,
-      include_package_data=INCLUDE_PACKAGE_DATA, version=VERSION, entry_points=ENTRY_POINTS,
-      install_requires=INSTALL_REQUIRES, classifiers=CLASSIFIERS, url=URL, cmdclass=CMDCLASS)
+THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(THIS_DIR, "README.md")) as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="wifiphisher",
+    author="sophron",
+    author_email="sophron@latthi.com",
+    description="Automated phishing attacks against Wi-Fi networks",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="GPL",
+    keywords=["wifiphisher", "evil", "twin", "phishing"],
+    packages=find_packages(exclude=["docs", "tests"]),
+    include_package_data=True,
+    version="1.4",
+    python_requires="~=2.7",
+    url="https://github.com/wifiphisher/wifiphisher",
+    project_urls={
+        "Documentation": "http://wifiphisher.readthedocs.io/en/latest/",
+        "Source": "https://github.com/wifiphisher/wifiphisher/",
+        "Tracker": "https://github.com/wifiphisher/wifiphisher/issues",
+    },
+    entry_points={
+        "console_scripts": ["wifiphisher = wifiphisher.pywifiphisher:run"]
+    },
+    install_requires=[
+        "PyRIC", "tornado==4.5.3", "pbkdf2", "roguehostapd", "scapy"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Natural Language :: English", "Operating System :: Unix",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only", "Topic :: Security",
+        "Topic :: System :: Networking",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology"
+    ],
+    cmdclass={
+        "clean": CleanCommand,
+    })
 
 get_dnsmasq()
 
