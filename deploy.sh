d24dba1df038880838e5c6c90a12da2ed14822f0
Brian Smith <brian.smith@riseup.net>
2018-06-23 11:53:02 -0400

    chore:add automatic release

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,11 @@ set -o pipefail
 #set -o xtrace
 
 main(){
+  git checkout --quiet master
   local current_version
   current_version=$(grep -i "version =" setup.py |
-    cut -d= -f2 |cut -d"\"" -f2)
-  echo -e "\u25b7\u25b7 Current Version = \e[91m${current_version}\e[39m"
+    cut -d= -f2 | tr -d "\" ")
+  echo -e "\\u25b7\\u25b7 Current Version = \\e[91m${current_version}\\e[39m"
 
   local new_version
   local major
@@ -22,40 +23,40 @@ main(){
   logs="$(git log "$(git describe --tags --abbrev=0)"..HEAD)"
 
   if echo "${logs}" | grep -iq "breaking change";then
-    echo -e "\e[41mBreaking Change Detected!\e[49m"
+    echo -e "\\e[41mBreaking Change Detected!\\e[49m"
     new_version="$((major + 1)).0.0"
   elif echo "${logs}" | grep -q "feat:"; then
-    echo -e "\e[41mNew Feature Detected!\e[49m"
+    echo -e "\\e[41mNew Feature Detected!\\e[49m"
     new_version="${major}.$((minor + 1)).0"
   elif echo "${logs}" | grep -q "fix:"; then
-    echo -e "\e[41mNew Fix Detected!\e[49m"
+    echo -e "\\e[41mNew Fix Detected!\\e[49m"
     new_version="${major}.${minor}.$((patch + 1))"
   else
-    echo -e "\e[41mNo Version Change!\e[49m"
+    echo -e "\\e[41mNo Version Change!\\e[49m"
     exit 0
   fi
 
-  echo -e "\u25b7\u25b7 New Version = \e[92m${new_version}\e[39m"
+  echo -e "\\u25b7\\u25b7 New Version = \\e[92m${new_version}\\e[39m"
 
-  echo -e "\u25b7\u25b7 Changing Version Numbers"
-  #sed -ie "s/$current_version/$new_version/" setup.py
-  echo -e "\e[92m\u2714\e[39m Updated all Version Numbers"
+  echo -e "\\u25b7\\u25b7 Changing Version Numbers"
+  sed -i "s/$current_version/$new_version/" setup.py
+  sed -i "s/$current_version/$new_version/" wifiphisher/pywifiphisher.py
 
-  echo -e "\e[93m\u2744\e[0m Generating New Changelog"
+  echo -e "\\e[93m\\u2744\\e[0m Generating New Changelog"
   local name_url="https://frightanic.com/goodies_content/docker-names.php"
-  local code_name=$(curl -s "$name_url")
-  echo -e "\u25b7\u25b7 New Version Code Name Is \e[42m\e[30m${code_name}\
-  \e[39m\e[49m"
-  echo "${new_version} ${code_name} ($(date +"%F"))" \
-    >> temp_changelog.md
-  printf '=%.0s' {1..60} >> temp_changelog.md
+  local code_name
+  code_name="$(curl -s "$name_url")"
+  echo -e "\\u25b7\\u25b7 New Version Code Name Is \\e[42m\\e[30m${code_name}\
+\\e[39m\\e[49m"
+  echo "${new_version} ${code_name} ($(date +"%F"))" >> temp_changelog.md
+  echo "=====" >> temp_changelog.md
   echo >> temp_changelog.md
 
   if echo "${logs}" | grep -iq "breaking change"; then
-    echo "Breaking Change:" >> temp_changelog.md
-    echo "-----------------" >> temp_changelog.md
-    echo "${logs}" | grep -i "breaking change:" | cut -d: -f2 | sed 's/^ //' \
-      >> temp_changelog.md
+    echo "Breaking Change" >> temp_changelog.md
+    echo "----" >> temp_changelog.md
+    echo "${logs}" | grep -i 'breaking change:' | cut -d: -f2 |
+    sed 's/^ //' >> temp_changelog.md
     echo >> temp_changelog.md
   fi
 
@@ -66,7 +67,7 @@ main(){
 
   if echo "${logs}" | grep -q "feat:"; then
     echo "New Features:" >> temp_changelog.md
-    echo "-----------------" >> temp_changelog.md
+    echo "-----" >> temp_changelog.md
 
     for hash in $commit_hashes;do
       message=$(git log -n 1 --pretty=format:%s "${hash}")
@@ -80,7 +81,7 @@ main(){
 
   if echo "${logs}" | grep -q "fix:"; then
     echo "Bug Fixes:" >> temp_changelog.md
-    echo "-----------------" >> temp_changelog.md
+    echo "-----" >> temp_changelog.md
     for hash in $commit_hashes;do
       message=$(git log -n 1 --pretty=format:%s "${hash}")
       if [[ $message =~ ^fix:.*$ ]];then
@@ -91,19 +92,36 @@ main(){
     echo >> temp_changelog.md
   fi
 
-  echo -e "$(cat temp_changelog.md)\n$(cat changelog.md)" > changelog.md
-  rm temp_changelog.md
+  echo -e "\\e[93m\\u2744\\e[0m Uploading to Github"
+  git config --global user.name "release-bot"
+  git config --global user.email "release-bot@github.com"
+  local github_url="https://${GH_TOKEN}@github.com/blackHatMonkey/\
+  wifiphisher.git"
+  git remote add new "${github_url// /}" 
+  git add wifiphisher/pywifiphisher.py setup.py changelog.md > /dev/null
+  git commit -m "chore:change release to ${new_version}" --quiet
+  git tag -a "v$new_version" -m "v$new_version"
+  git push new master --quiet
+  git push new --tags --quiet
 
-  echo -e "\e[92m\u2714\e[39m New Changelog generated"
-  echo -e "\e[93m\u2744\e[0m Uploading to Github"
-  #git config --global user.name "release-bot"
-  #git config --global user.email "release-bot@github.com"
-  #git add wifiphisher/ setup.py
-  #git commit -m "chore:change release to ${new_version}"
-  #git tag -a "v$new_version" -m "v$new_version"
-  #git push --tags --dry-run
-  #git push --dry-run
+  # the sed in the data section removes all end of file ($) abd repleaces
+  # it with \n since json can only be only line
+  curl --silent --show-error -H "Authorization: token ${GH_TOKEN}" \
+  https://api.github.com/repos/blackhatmonkey/wifiphisher/releases -d \
+  "{
+    \"tag_name\": \"${new_version}\",
+    \"name\": \"${new_version} ${code_name}\",
+    \"body\": \"$(sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' temp_changelog.md)\",
+    \"draft\": false,
+    \"prerelease\": false
+  }" > /dev/null
 
+  echo -e "$(cat temp_changelog.md)\\n\\n$(cat changelog.md)" > changelog.md
+
+  echo -e "\\u25b7\\u25b7 Uploading to PYPI"
+  python setup.py sdist > /dev/null
+  twine upload --repository-url https://test.pypi.org/legacy/ dist/* \
+  -u "${PYPI_USERNAME}" -p "${PYPI_PASS}" > /dev/null
 }
 
 main
